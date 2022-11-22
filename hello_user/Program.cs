371171cf6d1c5e9dd66503c0966129f18c53ad13
Anna Vasilchenko <annavasilcenko482@gmail.com>
2022-11-22 13:53:36 +0300

    Добавляет приветствие с приглашением

---
+++ hello_user/Program.cs
@@ -0,0 +1,12 @@
+﻿// Console.Write запись на одной строке
+Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, " + username);
+
+// Console.WriteLine запись на каждой строке
+// Console.WriteLine("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, " + username);
+
+
+
