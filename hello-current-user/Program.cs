c274f9ba23797690b1c0477aaa64523e0fd5ad6b
Anna Vasilchenko <annavasilcenko482@gmail.com>
2022-11-22 15:56:05 +0300

    Добавляет программу приветствия зарегестрированного пользователя

---
+++ hello-current-user/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите имя пользователя: ");
+string user = Console.ReadLine();
+if (user.ToLower() == "jane") {
+  Console.WriteLine("Ура это же Jane!");
+} else {
+  Console.Write("Привет, " + user);
+}
